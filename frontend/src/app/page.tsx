9b3eddf5f25455f0920cdabd74a3e09ba082b625
neeraj <neerajpc799@gmail.com>
2026-01-30 10:43:06 +0530

    Changes in sales page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,15 +6,18 @@ import DealCardContainer from "@/components/deal/DealCardContainer";
 const page = () => {
   return (
     <DashboardLayout>
-      <div className="grid grid-cols-1 md:flex gap-4 px-10">
+      <div className="flex flex-col md:flex-row gap-4 px-10">
         {/* LEFT SIDE */}
         <div className="w-full md:w-3/4 flex flex-col gap-4 pl-10">
           <CompanyCard />
-          <SalesTable />
+
+          <div className="min-h-[300px]">
+            <SalesTable />
+          </div>
         </div>
 
         {/* RIGHT SIDE */}
-        <div className="flex justify-center md:w-1/4">
+        <div className="flex justify-center md:w-1/4 min-h-[400px]">
           <DealCardContainer />
         </div>
       </div>
